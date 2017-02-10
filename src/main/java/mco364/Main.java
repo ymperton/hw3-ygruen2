9e2e56a014ea317096195f42fe24a094cd128254
Yehuda <Yehuda@yehudas-air.home>
2017-02-10 16:20:57 -0500

    HW 3 1.0

--- src/main/java/mco364/Main.java
+++ src/main/java/mco364/Main.java
@@ -1,61 +1,86 @@
 package mco364;
 
-
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 
 class MyThread extends Thread {    // the entry point of a thread is run
 
-    final int LOOP_MAX = 1000000;
-    int instanceCouter;
-    static int sharedCounter;
+    private static Random numberGen = new Random();
+    public static double sharedAverage;
+    int LOOP_MAX = 0; 
+    long total;
+    double average;
+
+    MyThread(int number) {
+        LOOP_MAX =  (100000000 / number);
+    }
 
     @Override
     public void run() {
+
         for (int i = 0; i < LOOP_MAX; i++) {
-            instanceCouter++;
-            synchronized("BathroomKey"){ // only one thread may enter at a time
-            MyThread.sharedCounter++;
-            }
-            //System.out.println("T2: " + i);
-            //Main.threadSleep(100);
+            total += numberGen.nextInt(100) + 1;
         }
+
+        average = (double) total / LOOP_MAX;
+
+        synchronized ("BathroomKey") { // only one thread may enter at a time
+            MyThread.sharedAverage += average;
+        }
+
     }
 
 }
 
-public class Main {
+class StopWatch {
 
-    public static void threadSleep(int ms) {
-        try {
-            Thread.sleep(ms);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    long startTime;
+
+    public void startTimer() {
+        startTime = System.currentTimeMillis();
     }
+
+    /**
+     * returns the number of milliseconds since startTimer was called. If
+     * startTimer was never called, or stopTimer was already called once, throws
+     * an TimerException (a subclass of RuntimeException) with an appropriate
+     * message
+     */
+    //Deal with above exceptions זך
+    //Change return type back to int
+    public double stopTimer() {
+        return (System.currentTimeMillis() - startTime) / 1000.0;
+    }
+}
+
+public class Main {
 
     public static void main(String[] args) {
 
+        int numberOfThreads = 10;
+        StopWatch timer = new StopWatch();
+
         ArrayList<MyThread> threadList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            MyThread t = new MyThread();
+        timer.startTimer();
+        for (int i = 0; i < numberOfThreads; i++) {
+            MyThread t = new MyThread(numberOfThreads);
             threadList.add(t);
             t.start();
         }
-        for(MyThread t : threadList){
+
+        for (MyThread t : threadList) {
             try {
                 t.join(); // wait for each thread to die (= complete) before continuting
             } catch (InterruptedException ex) {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
-        for(MyThread t : threadList){
-            System.out.println(t.instanceCouter + ", " + t.sharedCounter);
-        }
-        
+
+        //double avg = MyThread.sharedAverage / numberOfThreads;
+        System.out.println(MyThread.sharedAverage / numberOfThreads);
+        System.out.println(timer.stopTimer());
+
     }
 }
